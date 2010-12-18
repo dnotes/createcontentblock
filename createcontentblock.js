4ed5ec116edc48971eeeb1895a8b256ff434392f
David Hunt <davidhuntatwork@gmail.com>
2010-12-17 21:20:53 -0500

    Fixing css for cached blocks, variable saves, uninstallation of module, javascript handling for separate blocks, and block display for non-javascript blocks

--- createcontentblock.js
+++ createcontentblock.js
@@ -1,14 +1,16 @@
 Drupal.behaviors.createcontentblock = function (context) {
    // Togglable blocks
-  $('div.block-createcontentblock:not(.processed)')
+  $('div.block-createcontentblock:has(ul.createcontentblock-links.collapse):not(.processed)')
     .addClass('processed')
     .each(function() {
-      $('.block-title', this).click(function() {
+      $('.block-title', this).prepend('<span class="icon">+</span>').click(function() {
         if (!$(this).is('.toggle-active')) {
           $(this).addClass('toggle-active').siblings('div.content').show();
+          $(this).children('span.icon').text('\u2212');
         }
         else {
           $(this).removeClass('toggle-active').siblings('div.content').hide();
+          $(this).children('span.icon').text('+');
         }
         return false;
       });
